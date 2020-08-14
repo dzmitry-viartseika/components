f21c2aa05a4eaed9dc7463e81d071ec531219896
Dmitry Verteyko <verteyko1990@gmail.com>
2020-08-14 23:03:52 +0300

    added dropdown component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
   <div id="app">
     <h1>Components</h1>
+    <dropdown :dropdownOptions="dropdownOptions"/>
   </div>
 </template>
 
 <script>
+import dropdown from './components/elements/dropdown.vue';
 
 export default {
   name: 'App',
+  components: {
+    dropdown,
+  },
+  computed: {
+    dropdownOptions() {
+      return {
+        list: [
+          {
+            code: 'Ru',
+            text: 'Russian',
+          },
+          {
+            code: 'En',
+            text: 'English',
+          },
+        ],
+      };
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+  @import "sass/core";
 
 </style>
