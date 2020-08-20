557c103e9a880c732d0025fccd52da82bd04d47e
Dmitry Verteyko <verteyko1990@gmail.com>
2020-08-20 20:28:31 +0300

    add cookies notification

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,30 @@
       <h2>Button</h2>
       <my-button :buttonOptions="buttonOptions"/>
     </div>
+    <cookie-law>
+      <div slot-scope="props" class="cookies">
+        <div class="cookies-info">
+          <div class="cookies__text">
+            Мы используем cookie-файлы.
+            Продолжая использовать сайт, вы соглашаетесь на работу с этими файлами.
+            <a href="#" class="cookies__link">Что это значит?</a>
+          </div>
+        </div>
+        <div class="cookies-btns">
+          <button class="cookies-btns__btn cookies-btns__btn_border" @click="props.close">
+            <span>Нет, спасибо</span>
+          </button>
+          <button class="cookies-btns__btn cookies-btns__btn_primary" @click="props.accept">
+            <span>Окей</span>
+          </button>
+        </div>
+      </div>
+    </cookie-law>
   </div>
 </template>
 
 <script>
+import CookieLaw from 'vue-cookie-law';
 import dropdown from './components/elements/dropdown.vue';
 import buttonComponent from './components/elements/buttonComponent.vue';
 
@@ -20,6 +40,7 @@ export default {
   components: {
     dropdown,
     myButton: buttonComponent,
+    CookieLaw,
   },
   computed: {
     dropdownOptions() {
@@ -53,10 +74,6 @@ export default {
   },
 };
 </script>
-
-<style>
-  @import '../node_modules/vueperslides/dist/vueperslides.css';
-</style>
 
 <style lang="scss">
   @import "sass/core";
