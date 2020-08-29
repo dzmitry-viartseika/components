96ede04ad9f8d1e1d66c0877500c633fedaa1de9
Dmitry Verteyko <verteyko1990@gmail.com>
2020-08-29 20:41:41 +0300

    add accordion component

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,13 @@
       <my-button :buttonOptions="buttonOptions"/>
     </div>
     <cookieNotification v-if="!cookieToken" :cookieToken.sync="cookieToken"/>
+    <accordion  :items="items"/>
   </div>
 </template>
 
 <script>
 import dropdown from './components/elements/dropdown.vue';
+import accordion from './components/elements/accordion.vue';
 import cookieNotification from './components/cookieNotification.vue';
 import buttonComponent from './components/elements/buttonComponent.vue';
 
@@ -22,10 +24,25 @@ export default {
   components: {
     dropdown,
     cookieNotification,
+    accordion,
     myButton: buttonComponent,
   },
   data: () => ({
     cookieToken: false,
+    items: [{
+      id: 1,
+      title: 'Competition law',
+      content: 'wer 1Schärer Attorneys at Law advises and represents you on questions of unfair competition and the anti-trust law, for example, for company mergers, anti-trust investigations and for the drafting of distribution agreements.',
+    }, {
+      id: 2,
+      title: 'Constitutional, community and administrative law',
+      content: 'test 2 Civil law regulates privities of contract between private persons, communities of persons and corporations. On the other hand, constitutional, community and administrative law is concerned with the legal relationship between a private person and the community sector (federation, cantons, communities, associations of communities), or amongst communities. The specialists at Schärer Attorneys at Law act as advisers and consultants for private persons as well as communities, and represent them in the legal proceedings of objection and appeal.',
+    },
+    {
+      id: 3,
+      title: 'Construction, planning and environmental law',
+      content: 'greee 3Our specialists in the fields of construction, planning and environmental law advise and represent builders, planners and architects, corporations, affected neighboring communities and associations of communities in:',
+    }],
   }),
   computed: {
     dropdownOptions() {
